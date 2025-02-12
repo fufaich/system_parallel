4a5e72c4624331899cd3eb458e7101b204da1c5e
fufaich <fufaich@pop-os.lan>
2025-02-13 00:15:21 +0300

    Complete lab2

--- lab2/lab2.c
+++ lab2/lab2.c
@@ -1,50 +1,56 @@
 #include <mpi.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
-#define M 5
-#define N 4
+#define VECTOR_SIZE 20
+#define NUM_PROCESSES 4
 
-int main(int argc, char *argv[]) {
-   int rank, size;
-   
-   MPI_Init(&argc, &argv);
-   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+int main(int argc, char **argv) {
+    MPI_Init(&argc, &argv);
 
-   int gather_vector[N][M];
+    int rank, size;
+    long long result = 1;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-   if (rank == 0) {
-       MPI_Comm_size(MPI_COMM_WORLD, &size);
-       printf("Total size %d\n", size);
-       int cnt = 0;
-       for (int i = 0; i < N; i++) {
-           for (int j = 0; j < M; j++) {
-               gather_vector[i][j] = cnt++;
-           }
-       }
 
-       printf("Root generate vector:\n");
-       for (int i = 0; i < N; i++) {
-           for (int j = 0; j < M; j++) {
-               printf(" %d ", gather_vector[i][j]);
-           }
-           printf("\n");
-       }
-   }
+    short vector[VECTOR_SIZE];
+    srand(time(NULL) + rank);
+    
+    // Генерация чисел от 1 до 3 для избежания переполнения
+    printf("Proc %d vector: [", rank);
+    for (int i = 0; i < VECTOR_SIZE; i++) {
+        vector[i] = rand() % 5 + 1;
+        printf(" %d ", vector[i]);
+    }
+    printf("]\n");
 
-   MPI_Bcast(gather_vector, N * M, MPI_INT, 0, MPI_COMM_WORLD);
+    long long local_res = 1;
+    for (int i = 0; i < VECTOR_SIZE; i++) {
+        local_res *= vector[i];
+    }
 
-   if (rank != 0) {
-       printf("Process %d received message:\n", rank);
-       for (int i = 0; i < N; i++) {
-           for (int j = 0; j < M; j++) {
-               printf(" %d ", gather_vector[i][j]);
-           }
-           printf("\n");
-       }
-   }
+    long long *results = NULL;
+    if (rank == 0) {
+        results = malloc(NUM_PROCESSES * sizeof(long long));
+    }
 
-   MPI_Barrier(MPI_COMM_WORLD);
-   MPI_Finalize();
-   return 0;
+    MPI_Gather(&local_res, 1, MPI_LONG_LONG,
+               results, 1, MPI_LONG_LONG,
+               0, MPI_COMM_WORLD);
+
+    // Вывод результатов
+    if (rank == 0) {
+        printf("Res multiply:\n");
+        for (int i = 0; i < NUM_PROCESSES; i++) {
+            printf("Процесс %d: %lld\n", i, results[i]);
+            result *= results[i];
+        }
+        free(results);
+        printf("\nResult: %lld\n", result);
+    }
+
+    MPI_Finalize();
+    return 0;
 }
